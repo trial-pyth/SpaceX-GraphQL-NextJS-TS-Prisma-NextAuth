bb1e86fc2c0ae906f99c7267b6cbbf36f11f16b5
User <user@windows.com>
2023-03-02 20:20:53 +0530

    modified react query provider location

--- pages/explore/[explore].tsx
+++ pages/explore/[explore].tsx
@@ -1,17 +1,9 @@
 import ExploreLayout from "./components/ExploreLayout";
-import { QueryClientProvider } from "react-query";
-import { QueryClient } from "react-query";
 
 type Props = {};
 
-const queryClient = new QueryClient();
-
-const index = (props: Props) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ExploreLayout />
-    </QueryClientProvider>
-  );
+const index = () => {
+  return <ExploreLayout />;
 };
 
 export default index;
--- pages/explore/components/ExploreLayout.tsx
+++ pages/explore/components/ExploreLayout.tsx
@@ -2,12 +2,15 @@ import Data from "@/components/data/Data";
 import ExploreTab from "@/components/explorer-tab/ExploreTab";
 import React from "react";
 import { useRouter } from "next/router";
-
+import { QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 type ExploreLayoutProps = {
   // any props that come into the component
   queryItem?: string | undefined;
   itemInfo?: string;
 };
+
+const queryClient = new QueryClient();
 
 const ExploreLayout = ({ queryItem, itemInfo }: ExploreLayoutProps) => {
   // console.log(typeof exploreItem);
@@ -17,7 +20,9 @@ const ExploreLayout = ({ queryItem, itemInfo }: ExploreLayoutProps) => {
     <>
       <div className="explore-page mt-16 flex">
         <ExploreTab />
-        <Data queryItem={router.query.explore} />
+        <QueryClientProvider client={queryClient}>
+          <Data queryItem={router.query.explore} />
+        </QueryClientProvider>
       </div>
     </>
   );
