732361c9e87e3c6592564392b049f6516da911b1
User <user@windows.com>
2023-03-02 20:14:54 +0530

    modified next.config.js to ignore typescript error during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   images: {
     domains: ["live.staticflickr.com", "i.imgur.com", "i.ytimg.com"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
